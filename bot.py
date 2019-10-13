542e632936d005ae11d90002a83af60ef49d16e2
PropFeds <propfeds@gmail.com>
2019-10-13 15:31:06 +0700

    Wiki entries

--- bot.py
+++ bot.py
@@ -11,6 +11,9 @@ commands_wiki=load(open('wiki.json'))
 bot=commands.Bot(command_prefix=os.getenv('COMMAND_PREFIX'))
 guild_emoji={}
 
+def format_emoji(name):
+    return '<:{0}:{1}>'.format(name, guild_emoji[name])
+
 @bot.event
 async def on_ready():
     guild=bot.get_guild(guild_propane)
@@ -18,7 +21,9 @@ async def on_ready():
     for emoji in guild.emojis:
         guild_emoji[emoji.name]=emoji.id
     
-    print(f'{bot.user.name}, roll out! Entering {guild.name}(id: {guild.id})')
+    # Debog
+    print(guild_emoji)
+    print('{0} the {1}, roll out! Entering: {2} (id: {3})'.format(guild.me.display_name, bot.user.name, guild.name, guild.id))
 
 @bot.event
 async def on_message(message):
@@ -26,12 +31,12 @@ async def on_message(message):
         pass
     
     if 'wotcher' in message.content.lower():
-        await message.add_reaction('<:wotcher:{0}>'.format(guild_emoji['wotcher']))
+        await message.add_reaction(format_emoji('wotcher'))
 
     await bot.process_commands(message)
 
 @bot.command()
 async def wiki(context, entry):
-    await context.send('{0}: {1}'.format(entry, commands_wiki[entry.lower()]))
+    await context.send('{0}: {1}'.format(entry, commands_wiki.get(entry.lower(), 'Entry does not exist.')))
 
 bot.run(token)
