84187d32e42dec4a864bee63bcc996f9142fe7da
PropFeds <propfeds@gmail.com>
2019-10-16 14:23:56 +0700

    Rolling dice!

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
-import os
 import discord
 from discord.ext import commands
-from json import load, dump
 from dotenv import load_dotenv
+from json import load, dump
+import os
+from random import randint
 
 load_dotenv()
 token=os.getenv('DISCORD_TOKEN')
@@ -11,6 +12,21 @@ commands_wiki=load(open('data/wiki.json', encoding='utf-8'))
 bot=commands.Bot(command_prefix=os.getenv('COMMAND_PREFIX'))
 guild_emoji={}
 guild_role_indexes={}
+
+def roll_die(dice, sides, mod):
+    results={}
+    results_dice=[]
+    results_sum=mod
+    for _ in range(dice):
+        roll=randint(1, sides)
+        results_dice.append(roll)
+        results_sum+=roll
+    results['dice']=results_dice
+    results['sum']=results_sum
+    return results
+
+def format_die_roll(results):
+    return '\n`{0}`→{1}'.format(results['dice'], results['sum'])
 
 def get_role(guild, name):
     return guild.roles[guild_role_indexes[name]]
@@ -57,7 +73,6 @@ async def wiki(context, *entries):
 
         if commands_wiki.get(entry.lower()):
             entry_cur=entry
-
             # Redirecting entries
             while commands_wiki.get(entry_cur)[0]=='>':
                 entry_cur=commands_wiki.get(entry_cur)[1:]
@@ -70,7 +85,6 @@ async def wiki(context, *entries):
 
 @bot.command(description='Bards you and unbards you.')
 async def bard(context):
-
     if get_role(context.guild, 'Bard') in context.author.roles:
         await context.author.remove_roles(get_role(context.guild, 'Bard'), reason="Unbarded by command")
         await context.send("Unbarded!")
@@ -78,4 +92,28 @@ async def bard(context):
         await context.author.add_roles(get_role(context.guild, 'Bard'), reason="Barded by command")
         await context.send("Barded!")
 
+@bot.command(description='Rolls dice with the xdy+m format. First word in reason can specify number of repeats.')
+async def roll(context, die, *reason):
+    nof_repeats=1
+    response="Rolling `{0}`".format(die)
+    for i, word in enumerate(reason):
+        if not i:
+            if word.isdigit():
+                nof_repeats=int(word)
+                response+=' {0} times for'.format(word)
+            else:
+                response+=' once for {0}'.format(word)
+        else:
+            response+=' {0}'.format(word)
+    response+=':'
+    # Replacing 'd' and '+' with spaces
+    die_list=die.translate({100:32, 43:32}).split()
+    die_list.append(0)
+    for _ in range(nof_repeats):
+        response+=format_die_roll(roll_die(int(die_list[0]), int(die_list[1]), int(die_list[2])))
+
+    await context.send(response)
+
 bot.run(token)
+
+# Todo: !roll but powered by random.org; !gungeon @mention time_hours reason: Muted!
