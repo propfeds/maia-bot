0ee69cab639a3048e2dc86e770d435cba516b70b
PropFeds <propfeds@gmail.com>
2019-11-01 01:16:24 +0700

    Cog-minded

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ with open('data/responses.json', encoding='utf-8') as json_responses:
 guild_emoji={}
 guild_role_ids={}
 guild_role_indexes={}
-bard_rare_chance=os.getenv('DISCORD_BARD_RARE_CHANCE')
+bard_rare_chance=int(os.getenv('DISCORD_BARD_RARE_CHANCE'))
 
 bot=commands.Bot(command_prefix=['!', '/', 'Maia, ', 'maia, ', 'MAIA, ', 'mAIA, '])
 
@@ -54,82 +54,86 @@ async def on_ready():
 
     print(responses['on_ready'].format(guild.me.display_name, bot.user.name, guild.name, guild.id))
 
-@bot.command(description=responses['bard']['desc'], help=responses['bard']['help'], brief=responses['bard']['brief'])
-async def bard(context):
-    if get_role(context.guild, 'Bard') in context.author.roles:
-        await context.author.remove_roles(get_role(context.guild, 'Bard'), reason=responses['bard']['unbard_reason'])
-        await context.send(responses['bard']['unbard'].format(context.author.display_name))
-    else:
-        await context.author.add_roles(get_role(context.guild, 'Bard'), reason=responses['bard']['bard_reason'])
-        roll_rare=randint(0, 99)
-        if roll_rare<bard_rare_chance:
-            await context.send(responses['bard']['bard_rare'].format(context.author.display_name, get_possessive(context.author.display_name)))
+class Queries(commands.Cog):
+    def __init__(self, bot):
+        self.bot=bot
+
+    @commands.command(description=responses['bard']['desc'], help=responses['bard']['help'], brief=responses['bard']['brief'])
+    async def bard(self, context):
+        if get_role(context.guild, 'Bard') in context.author.roles:
+            await context.author.remove_roles(get_role(context.guild, 'Bard'), reason=responses['bard']['unbard_reason'])
+            await context.send(responses['bard']['unbard'].format(context.author.display_name))
         else:
-            await context.send(responses['bard']['bard'].format(context.author.display_name))
-
-@bot.command(aliases=responses['calc']['aliases'], description=responses['calc']['desc'], help=responses['calc']['help'], brief=responses['calc']['brief'])
-async def calc(context, *exp):
-    exp_full=''.join(exp)
-    await context.send(choice(responses['calc']['result']).format(eval(str(exp_full))))
-
-@bot.command(description=responses['roll']['desc'], help=responses['roll']['help'], brief=responses['roll']['brief'])
-async def roll(context, die, *reason):
-    nof_repeats=1
-    response='{0} `{1}` for {2}'.format(responses['roll']['rolling'], die, context.author.display_name)
-    for i, word in enumerate(reason):
-        response+=' '
-        if not i:
-            if word.isdigit():
-                nof_repeats=int(word)
-                response+='{0} {1}'.format(word, responses['roll']['times'])
-                if len(reason)>1:
-                    response+=' {0}'.format(responses['roll']['for'])
+            await context.author.add_roles(get_role(context.guild, 'Bard'), reason=responses['bard']['bard_reason'])
+            roll_rare=randint(0, 99)
+            if roll_rare<bard_rare_chance:
+                await context.send(responses['bard']['bard_rare'].format(context.author.display_name, get_possessive(context.author.display_name)))
             else:
-                response+='{0} {1}'.format(responses['roll']['once_for'], word)
+                await context.send(responses['bard']['bard'].format(context.author.display_name))
+
+    @commands.command(aliases=responses['calc']['aliases'], description=responses['calc']['desc'], help=responses['calc']['help'], brief=responses['calc']['brief'])
+    async def calc(self, context, *exp):
+        exp_full=''.join(exp)
+        await context.send(choice(responses['calc']['result']).format(eval(str(exp_full))))
+
+    @commands.command(description=responses['roll']['desc'], help=responses['roll']['help'], brief=responses['roll']['brief'])
+    async def roll(self, context, die, *reason):
+        nof_repeats=1
+        response='{0} `{1}` for {2}'.format(responses['roll']['rolling'], die, context.author.display_name)
+        for i, word in enumerate(reason):
+            response+=' '
+            if not i:
+                if word.isdigit():
+                    nof_repeats=int(word)
+                    response+='{0} {1}'.format(word, responses['roll']['times'])
+                    if len(reason)>1:
+                        response+=' {0}'.format(responses['roll']['for'])
+                else:
+                    response+='{0} {1}'.format(responses['roll']['once_for'], word)
+            else:
+                response+=word
+        response+=':'
+
+        # Replacing 'd' and '+' with spaces
+        die_list=die.translate({100:32, 43:32}).split()
+        die_list.append('0')
+        dice, sides, mod=int(die_list[0]), int(die_list[1]), int(die_list[2])
+
+        results_dice=[]
+        try:
+            results_dice.extend(randorg_client.generate_integers(dice*nof_repeats, 1, sides))
+        except RandomOrgSendTimeoutError:
+            response+='\n{0}'.format(responses['roll']['randorg_timeout'])
+        except (RandomOrgInsufficientRequestsError, RandomOrgInsufficientBitsError):
+            response+='\n{0}'.format(responses['roll']['randorg_juice'])
+            for _ in range(dice*nof_repeats):
+                results_dice.append(randint(1, sides))
+
+        # Actually displaying dice
+        for i in range(nof_repeats):
+            roll=results_dice[dice*i:dice*(i+1)]
+            response+='\n`{0}`→{1}'.format(str(roll), sum(roll)+mod)
+
+        await context.send(response)
+
+    @commands.command(aliases=responses['wiki']['aliases'], description=responses['wiki']['desc'], help=responses['wiki']['help'], brief=responses['wiki']['brief'])
+    async def wiki(self, context, *entry):
+        entry_full=' '.join(entry).lower()
+
+        response='**{0}'.format(entry_full)
+        if commands_wiki.get(entry_full):
+            # Redirecting entries
+            while commands_wiki.get(entry_full)[0]=='>':
+                entry_full=commands_wiki.get(entry_full)[1:]
+                response+='→{0}'.format(entry_full)
+
+            response+=':** {0}'.format(commands_wiki.get(entry_full))
         else:
-            response+=word
-    response+=':'
+            response+=':** {0}'.format(responses['wiki']['entry_not_exist'])
 
-    # Replacing 'd' and '+' with spaces
-    die_list=die.translate({100:32, 43:32}).split()
-    die_list.append('0')
-    dice, sides, mod=int(die_list[0]), int(die_list[1]), int(die_list[2])
+        await context.send(response)
 
-    results_dice=[]
-    try:
-        results_dice.extend(randorg_client.generate_integers(dice*nof_repeats, 1, sides))
-    except RandomOrgSendTimeoutError:
-        response+='\n{0}'.format(responses['roll']['randorg_timeout'])
-    except (RandomOrgInsufficientRequestsError, RandomOrgInsufficientBitsError):
-        response+='\n{0}'.format(responses['roll']['randorg_juice'])
-        for _ in range(dice*nof_repeats):
-            results_dice.append(randint(1, sides))
-
-    # Actually displaying dice
-    for i in range(nof_repeats):
-        roll=results_dice[dice*i:dice*(i+1)]
-        response+='\n`{0}`→{1}'.format(str(roll), sum(roll)+mod)
-
-    await context.send(response)
-
-@bot.command(aliases=responses['wiki']['aliases'], description=responses['wiki']['desc'], help=responses['wiki']['help'], brief=responses['wiki']['brief'])
-async def wiki(context, *entry):
-    entry_full=' '.join(entry).lower()
-
-    response='**{0}'.format(entry_full)
-    if commands_wiki.get(entry_full):
-        # Redirecting entries
-        while commands_wiki.get(entry_full)[0]=='>':
-            entry_full=commands_wiki.get(entry_full)[1:]
-            response+='→{0}'.format(entry_full)
-
-        response+=':** {0}'.format(commands_wiki.get(entry_full))
-    else:
-        response+=':** {0}'.format(responses['wiki']['entry_not_exist'])
-
-    await context.send(response)
-
-
+bot.add_cog(Queries(bot))
 bot.add_cog(Fluff(bot, guild_emoji, responses))
 bot.run(os.getenv('DISCORD_TOKEN'))
 
