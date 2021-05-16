55eed305b2441fe61ff1f029a0da616dc1bda06d
Minh <propfeds@gmail.com>
2021-05-16 15:04:52 +0900

    Bot reports running cogs to status

--- bot.py
+++ bot.py
@@ -1,15 +1,21 @@
+import asyncio
+from asyncio.events import AbstractEventLoop
 import cogs
 from cogs.core import Core
 from discord.ext import commands
 from dotenv import load_dotenv
 from os import getenv
 
+async def autorun(bot: commands.Bot, cogs: list) -> None:
+    core: commands.Cog=bot.get_cog('Core')
+    tasks=[core.construct_cog(cog, False) for cog in cogs]
+    await asyncio.wait(tasks)
+
 bot: commands.Bot=commands.Bot(command_prefix=cogs._global['prefixes'])
 
 bot.add_cog(Core(bot))
-core=bot.get_cog('Core')
-for cog in cogs._global['autoruns']:
-    core.construct_cog(cog)
+loop: AbstractEventLoop=asyncio.get_event_loop()
+run=loop.run_until_complete(autorun(bot, cogs._global['autoruns']))
 
 load_dotenv()
 bot.run(getenv('DISCORD_TOKEN'))
--- cogs/core.py
+++ cogs/core.py
@@ -23,7 +23,14 @@ class Core(commands.Cog):
         }
         self._running_cogs: set=set()
 
-    def construct_cog(self, name: str) -> str:
+    async def report_cogs(self) -> None:
+        cog_list: list=[]
+        for cog in self._running_cogs:
+            cog_list.append(cog.__name__)
+        await self.bot.change_presence(activity=discord.Game(
+            ', '.join(cog_list)))
+
+    async def construct_cog(self, name: str, online: bool) -> str:
         if not self._cog_aliases.get(name):
             # This one prints to console because it's related to autoruns
             print(f'Load failed: cog name \'{name}\' cannot be found.')
@@ -38,10 +45,11 @@ class Core(commands.Cog):
             else:
                 self._running_cogs.add(cog_type)
                 self.bot.add_cog(cog_type(self.bot))
-
+                if online:
+                    await self.report_cogs()
                 return None
 
-    def destroy_cog(self, name: str) -> str:
+    async def destruct_cog(self, name: str) -> str:
         if not self._cog_aliases.get(name):
             # No need to print
             # print(f'Termination failed: cog name \'{name}\' cannot be found.')
@@ -55,7 +63,7 @@ class Core(commands.Cog):
             else:
                 self._running_cogs.remove(cog_type)
                 self.bot.remove_cog(cog_type.__name__)
-
+                await self.report_cogs()
                 return None
 
     def load_guild_cfg(self, guild: discord.Guild) -> None:
@@ -77,10 +85,7 @@ class Core(commands.Cog):
 
         print(f'{self.bot.user.name}, rolling out in the age of {discord.__version__}!')
 
-        if cogs._global.get('status'):
-            game: str=cogs._global['status']
-            await self.bot.change_presence(activity=discord.Game(game))
-            print(f'Playing: {game}')
+        await self.report_cogs()
 
     @commands.command(**cogs._cmd['play'])
     async def play(self, ctx: commands.Context, *title: str) -> None:
@@ -88,7 +93,7 @@ class Core(commands.Cog):
         if title_full in ('Debug', 'debug'):
             await self.bot.change_presence(status=discord.Status.dnd)
             return
-        response=self.construct_cog(title_full)
+        response=await self.construct_cog(title_full, True)
         if response:
             await ctx.send(response)
     
@@ -98,47 +103,6 @@ class Core(commands.Cog):
         if title_full in ('Debug', 'debug'):
             await self.bot.change_presence(status=discord.Status.online)
             return
-        response=self.destroy_cog(title_full)
+        response=await self.destruct_cog(title_full)
         if response:
             await ctx.send(response)
-    
-    # @commands.command(**cogs._cmd['play'])
-    # async def play(self, ctx: commands.Context, *game: str) -> None:
-    #     role_botkeep: discord.Role=cogs.get_role(ctx.guild,
-    #         cogs._guild[ctx.guild.id]['botkeep'])
-    #     if role_botkeep not in ctx.author.roles:
-    #         await ctx.send(cogs._resp['play']['403'])
-    #         return
-    # 
-    #     game_full: str=' '.join(game)
-    # 
-    #     if game_full=='':
-    #         await ctx.send(cogs._resp['play']['404'])
-    #         return
-    #     elif game_full in ('Debug', 'debug'):
-    #         await self.bot.change_presence(activity=discord.Game('Debug'),
-    #             status=discord.Status.dnd)
-    #         cogs._global['status']='Debug'
-    #         with open('data/config.json', 'w+', encoding='utf-8'
-    #         ) as json_global:
-    #             dump(cogs._global, json_global, sort_keys=True, indent=4)
-    #     elif game_full in ('Nothing, nothing'):
-    #         await self.bot.change_presence(activity=None,
-    #             status=discord.Status.online)
-    #         # Export status for use next launch
-    #         prev_game=cogs._global.pop('status')
-    #         await ctx.send(cogs._resp['play']['nothing'].format(prev_game))
-    #         with open('data/config.json', 'w+', encoding='utf-8'
-    #         ) as json_global:
-    #             dump(cogs._global, json_global, sort_keys=True, indent=4)
-    #     else:
-    #         if cogs._resp['play'].get(game_full):
-    #             await ctx.send(cogs._resp['play'][game_full])
-    #         await self.bot.change_presence(activity=discord.Game(game_full),
-    #             status=discord.Status.online)
-    #         # Export status for use next launch
-    #         cogs._global['status']=game_full
-    #         with open('data/config.json', 'w+', encoding='utf-8'
-    #         ) as json_global:
-    #             dump(cogs._global, json_global, sort_keys=True, indent=4)
-
