6888a6c0273191c44fe8417b2c5aa37e942d4cd4
Minh <propfeds@gmail.com>
2021-12-26 01:27:18 +0900

    Fixed empty coroutine error with no autorun tasks

--- bot.py
+++ bot.py
@@ -15,8 +15,9 @@ bot: commands.Bot=commands.Bot(command_prefix=cogs._global['prefixes'],
     case_insensitive=True)
 
 bot.add_cog(Core(bot))
-loop: AbstractEventLoop=asyncio.get_event_loop()
-run=loop.run_until_complete(autorun(bot, cogs._global['autoruns']))
+if cogs._global['autoruns']!=[]:
+    loop: AbstractEventLoop=asyncio.get_event_loop()
+    run=loop.run_until_complete(autorun(bot, cogs._global['autoruns']))
 
 load_dotenv()
 bot.run(getenv('DISCORD_TOKEN'))
